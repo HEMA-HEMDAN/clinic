ad114c323fc030d6dfe11a9a83f45d0dfe0a61c4
Eng-HEMA <hshaabrahym36@gmail.com>
2025-11-27 15:52:32 +0200

    connecting to lacal db

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,24 +3,19 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
-// Sequelize connection to CockroachDB using COCKROACH_URL from .env
-const databaseUrl = process.env.COCKROACH_URL;
+// put your pass here
+const database = process.env.LOCAL_DATABASE_pass;
 
-if (!databaseUrl) {
-  throw new Error(
-    "COCKROACH_URL environment variable is not set.\n" +
-      "Please create a .env file in the root directory with:\n" +
-      "COCKROACH_URL=postgresql://user:password@host:port/database?sslmode=verify-full"
-  );
-}
+// if (!databaseUrl) {
+//   throw new Error(
+//     "COCKROACH_URL environment variable is not set.\n" +
+//       "Please create a .env file in the root directory with:\n" +
+//       "COCKROACH_URL=postgresql://user:password@host:port/database?sslmode=verify-full"
+//   );
+// }
 
-export const sequelize = new Sequelize(databaseUrl, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-  logging: false, // Set to console.log for debugging
+export const sequelize = new Sequelize("mydb", "root", database, {
+  host: "localhost",
+  dialect: "mysql",
+  logging: false, // disable console logs
 });
